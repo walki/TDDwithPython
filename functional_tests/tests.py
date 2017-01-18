10efe0823e84457d2991efffcddc784e1636d616
Roger Walker <roger.john.walker@gmail.com>
2017-01-17 21:38:46 -0500

    Moved view_list view and view.html for displaying lists, but only one list URL as of right now

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,6 +44,10 @@ class NewVisitorTest(LiveServerTestCase):
         # and now the page lists:
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
+
+        import time
+        time.sleep(3)
+
         edith_list_url = self.browser.current_url
         self.assertRegex( edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -56,16 +60,47 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        time.sleep(3)
+
 
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site generates a unique URL for her -- there is some
-        # explanatory text to that effect.
+
+        # Now a new user, Francis, comes along to the sire.
+
+        ## We use a new browser session to make sure no information
+        ## of Edith's is coming from cookies, etc. #
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He is
+        # less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+
+        time.sleep(3)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's lists
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+
+        # Satisfied , they both goes back to sleep
         self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied , she goes back to sleep
--- lists/tests.py
+++ lists/tests.py
@@ -41,7 +41,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_home_page_only_saves_items_when_necessary(self):
@@ -50,34 +50,41 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text = 'itemey 1')
-        Item.objects.create(text = 'itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
 
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-            first_item = Item()
-            first_item.text = 'The first (ever) list item'
-            first_item.save()
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-            second_item = Item()
-            second_item.text = 'Item the second'
-            second_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
 
-            saved_items = Item.objects.all()
+        saved_items = Item.objects.all()
 
-            self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items.count(), 2)
 
-            first_saved_item = saved_items[0]
-            second_saved_item = saved_items[1]
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
 
-            self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-            self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+
+    def test_displays_all_items(self):
+        Item.objects.create(text = 'itemey 1')
+        Item.objects.create(text = 'itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
