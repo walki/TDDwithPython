8a6177865783f5509a039d1c219e55e9710d62ba
Roger Walker <roger.john.walker@gmail.com>
2017-01-25 21:43:13 -0500

    removed add_item view and url

--- lists/views.py
+++ lists/views.py
@@ -24,8 +24,3 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id = list_id)
-    Item.objects.create(text=request.POST['item_text'], list = list_)
-    return redirect('/lists/%d/' % (list_.id,))
